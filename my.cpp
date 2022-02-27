a63f6ecd675f98d2b8939288775a55e6658137b4
faizal khan <iamfaizalkhn@gmail.com>
2022-02-27 08:10:12 -0800

    cpp

---
+++ my.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr = 10;
+    cout<<arr;
+    return 0;
+}
---
+++ new.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< " this is going to get ignore";
+    return 0;
+}
